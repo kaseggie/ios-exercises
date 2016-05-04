5fc025131cc7be5b9326d3be341346c31d434a72
Kavan <kseggie@gmail.com>
2016-05-04 15:46:11 +0200

    not sure what I am doing here

---
+++ Car.h
@@ -0,0 +1,15 @@
+//
+//  Car.h
+//  BlocExercises
+//
+//  Created by Kavan Seggie on 14/04/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+- (void) honk;
+
+@end
---
+++ Car.m
@@ -0,0 +1,20 @@
+//
+//  Car.m
+//  BlocExercises
+//
+//  Created by Kavan Seggie on 14/04/2016.
+//
+//
+
+#import "Car.h"
+
+@implementation Car
+
+-(void) honk
+{
+    NSLog(@"Beep! Beep!");
+    
+}
+
+
+@end
